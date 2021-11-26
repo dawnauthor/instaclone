812b60e52e8812c5152c69109dae7cac7304131a
Darryl Taylor <contact.dtaylor@gmail.com>
2021-11-26 17:04:27 -0500

    fix: env

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 export const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	apiKey: "AIzaSyCjDBrag1Pi5oGMBY9TXxQ_wdJnT_4e3VE",
+	authDomain: "insta-clone-e34b0.firebaseapp.com",
+	projectId: "insta-clone-e34b0",
+	storageBucket: "insta-clone-e34b0.appspot.com",
+	messagingSenderId: "265301968209",
+	appId: "1:265301968209:web:f08fb9faf72e56f0a9ff70"
 };
 
 //Retrieve existing app if available, otherwise initialize app
